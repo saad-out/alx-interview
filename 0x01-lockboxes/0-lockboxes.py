eb6015a4fadf1b963986fe8a7a65b3f14cf33220
saad-out <outsaad03@gmail.com>
2023-04-28 11:37:15 +0100

    use iterative approach instead of recursion

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -17,33 +17,24 @@ def canUnlockAll(boxes):
     if not boxes or len(boxes) == 1:
         return True
 
-    return unlockBox(0, {}, boxes)
+    current_box = 0
+    boxes_keys = {}
+    next_box = True
 
+    while next_box:
+        boxes_keys[current_box] = True
+        for key in boxes[current_box]:
+            if key not in boxes_keys.keys():
+                boxes_keys[key] = False
 
-def unlockBox(current_box, boxes_keys, boxes):
-    """
-    Unlocks a box and checks if all boxes are unlocked
+        if len(boxes_keys) == len(boxes):
+            return True
 
-    Args:
-        current_box (int): current box to unlock
-        boxes_keys (dict): dictionary of boxes and their status
-        boxes (list): list of boxes
+        next_box = False
+        for key, unlocked in boxes_keys.items():
+            if (not unlocked) and (key < len(boxes) and key >= 0):
+                next_box = True
+                current_box = key
+                break
 
-    Return:
-        (bool) True if all boxes are unlocked, false otherwise
-    """
-    if current_box < 0 or current_box >= len(boxes):
-        raise IndexError("current_box out of range")
-
-    boxes_keys[current_box] = True
-    for key in boxes[current_box]:
-        if key not in boxes_keys.keys():
-            boxes_keys[key] = False
-
-    if len(boxes_keys) == len(boxes):
-        return True
-
-    for key, unlocked in boxes_keys.items():
-        if (not unlocked) and (key < len(boxes) and key >= 0):
-            return unlockBox(key, boxes_keys, boxes)
     return False
