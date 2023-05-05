e790f31826268236379f20b20095d43ebedf21e5
saad-out <outsaad03@gmail.com>
2023-05-05 23:45:53 +0100

    bring back old (docs not working with checker)

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,6 +6,41 @@ characters in the file.
 """
 
 
+def next_operation(
+                   target: int,
+                   operations: int,
+                   current_characters: int,
+                   add_by: int
+                   ) -> int:
+    """
+    Recursive function to calculate the next operation
+    Args:
+        target (int): The target number of characters
+        operations (int): The number of operations
+        current_characters (int): The current number of characters
+        add_by (int): The number of characters to add by
+    Returns:
+        int: The number of operations
+    """
+    if target == current_characters:
+        return operations
+
+    if target % current_characters == 0:
+        return next_operation(
+                              target,
+                              operations + 2,
+                              current_characters * 2,
+                              current_characters
+                              )
+
+    return next_operation(
+                          target,
+                          operations + 1,
+                          current_characters + add_by,
+                          add_by
+                          )
+
+
 def minOperations(n: int) -> int:
     """
     Calculates the fewest number of operations needed to result in
@@ -19,20 +54,4 @@ def minOperations(n: int) -> int:
     if n <= 1:
         return 0
 
-    operations: int = 2
-    current_characters: int = 2
-    add_by: int = 1
-
-    while n != current_characters:
-        if n % current_characters == 0:
-            add_by = current_characters
-            current_characters *= 2
-            operations += 2
-        else:
-            current_characters += add_by
-            operations += 1
-
-        if current_characters > n:
-            return 0
-
-    return operations
+    return next_operation(n, 2, 2, 1)
