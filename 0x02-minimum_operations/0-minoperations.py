2fc01c234d5073589f8cd362b92e944d617b8f49
saad-out <outsaad03@gmail.com>
2023-05-05 23:35:32 +0100

    regenerate docs

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
+"""
+This module contains a function that calculates the minimum number of
+operations required to
+obtain a string of length n containing only the character 'A', using
+the following two operations:
+
+1. Add the character 'A' to the end of the string.
+2. Copy the entire string and paste it to the end of the current string.
+"""
 
 
 def minOperations(n: int) -> int:
+    """
+    Calculate the minimum number of operations required to obtain a
+    string of length n
+    containing only the character 'A', using the following two operations:
+
+    1. Add the character 'A' to the end of the string.
+    2. Copy the entire string and paste it to the end of the current string.
+
+    Args:
+    n (int): The length of the target string.
+
+    Returns:
+    int: The minimum number of operations required to obtain the
+        target string.
+    """
     if n <= 1:
         return 0
 
