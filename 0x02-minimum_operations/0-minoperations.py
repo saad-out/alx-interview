38937cefbe40710966db3672d8c700cd99a0fa08
saad-out <outsaad03@gmail.com>
2023-05-05 23:43:57 +0100

    real code with same docs

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -19,4 +19,20 @@ def minOperations(n: int) -> int:
     if n <= 1:
         return 0
 
-    return 0
+    operations: int = 2
+    current_characters: int = 2
+    add_by: int = 1
+
+    while n != current_characters:
+        if n % current_characters == 0:
+            add_by = current_characters
+            current_characters *= 2
+            operations += 2
+        else:
+            current_characters += add_by
+            operations += 1
+
+        if current_characters > n:
+            return 0
+
+    return operations
