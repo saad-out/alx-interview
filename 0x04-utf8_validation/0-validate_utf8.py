2fcfde6f5532ec98dc9c94f72d5067ec6f10193b
saad-out <outsaad03@gmail.com>
2023-05-18 13:47:22 +0100

    UTF-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+This module contains a function that determines if a given data set represents
+a valid UTF-8 encoding.
+"""
+BYTE = 8
+
+
+def one_byte(n):
+    """
+    Converts a number to a binary string representation of
+    least significant byte.
+
+    Args:
+        n (int): The number to convert.
+
+    Returns:
+        str: The binary string representation of the least significant byte.
+    """
+    bin_n = bin(n).split('b')[1]
+    if len(bin_n) >= BYTE:
+        return bin_n[-8:]
+    else:
+        leading_zeroes = ''
+        for i in range(BYTE - len(bin_n)):
+            leading_zeroes += '0'
+        return leading_zeroes + bin_n
+
+
+def bytes_count(byte):
+    """
+    Determines the number of remaining bytes in a UTF-8 character.
+
+    Args:
+        byte (str): The binary string representation of the least significant
+
+    Returns:
+        int: The number of remaining bytes in a UTF-8 character.
+    """
+    if not byte or type(byte) != str:
+        return 0
+
+    if byte.startswith('0'):
+        return 0
+    elif byte.startswith('110'):
+        return 1
+    elif byte.startswith('1110'):
+        return 2
+    elif byte.startswith('1111'):
+        return 3
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): The data set to validate.
+
+    Returns:
+        bool: True if the data set represents a valid UTF-8 encoding, False
+    """
+    continuation_byte = False
+    remaining_bytes = 0
+    for n in data:
+        byte_n = one_byte(n)
+        if not continuation_byte:
+            if byte_n.startswith('10'):
+                return False
+            remaining_bytes = bytes_count(byte_n)
+        else:
+            if not byte_n.startswith('10'):
+                return False
+            remaining_bytes -= 1
+        if remaining_bytes == 0:
+            continuation_byte = False
+        elif remaining_bytes >= 0:
+            continuation_byte = True
+
+    return True
